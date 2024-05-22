cead38b9628e5ef2be60d2d7088e233675fb7d9d
thanhtrinh1532 <thanhtrinh15032000@gmail.com>
2024-05-22 19:29:49 +0700

    Init Project

---
+++ src/edu/qlkho/AgileProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edu.qlkho;
+
+/**
+ *
+ * @author USER
+ */
+public class AgileProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
